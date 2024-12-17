d7fbf548b25871061b36b76a9c823b7786d1d427
Bread-ou <93944942+Bread-ou@users.noreply.github.com>
2024-12-17 10:22:32 +0300

    Add project files.

---
+++ Task2Conditions/Program.cs
@@ -0,0 +1,68 @@
+﻿// Reading name and language from user
+
+Console.WriteLine("Please enter your name: ");
+Console.WriteLine("----------------------- ");
+string name = Console.ReadLine();
+
+Console.WriteLine("Please enter your language: ");
+Console.WriteLine("--------------------------- ");
+string language  = Console.ReadLine();
+
+// Display 
+if (language == "en")
+{
+    Console.WriteLine("Hello " + name);
+}else if (language == "es")
+{
+    Console.WriteLine("Hola " + name);
+}
+else if (language == "fr")
+{
+    Console.WriteLine("Bonjour " + name);
+}
+else
+{
+    Console.WriteLine("Merhaba " + name);
+}
+
+// Max Number
+
+Console.WriteLine("Please enter a number: ");
+Console.WriteLine("--------------------- ");
+double num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter another number: ");
+Console.WriteLine("--------------------- ");
+double num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine(num1+ " is larger than "+ num2);
+}
+else
+{
+    Console.WriteLine(num2+ " is larger than "+ num1);
+}
+
+
+// Mark
+Console.WriteLine("Please enter your mark: ");
+Console.WriteLine("---------------------- ");
+var mark = Convert.ToInt32(Console.ReadLine());
+
+if (mark >= 88)
+{
+    Console.WriteLine("You got an A.");
+}else if (mark >= 70)
+{
+    Console.WriteLine("You got a B.");
+}else if (mark >= 60)
+{
+    Console.WriteLine("You got a C.");
+}else if (mark >= 50)
+{
+    Console.WriteLine("You got a D.");
+}else
+{
+    Console.WriteLine("You got an F.");
+}
